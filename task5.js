91e33a91fa430267d9d5893a9b5febaf8c3088ce
Zenande Nkolisa <38208747+lwetu@users.noreply.github.com>
2018-05-18 11:58:01 +0200

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,11 @@
+var myBox = document.getElementById("box1");
+var Button = document.getElementById("button1");
+var Button = document.getElementById("button1");
+
+Button.addEventListener('click', function(){
+	myBox.style.backgroundColor = 'green'
+Button.addEventListener('click', function(){
+	myBox.style.backgroundColor = 'red'
+})
+
+  
